f2c29b2469f726453253c7a8058d5591eeb1491c
Bennett Van Soelen <78813551+bennettvansoelen@users.noreply.github.com>
2021-03-24 09:41:29 -0500

    Can calculate surface area of top of tank

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
@@ -10,6 +10,20 @@ def index():
 @app.route('/mike')
 def mike():
     return render_template('mike.html')
+
+@app.route('/price', methods=['GET'])
+def price():
+    return render_template('price.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    if request.method == 'POST':
+        pi = 3.14
+        r = float(request.form['radius'])
+        h = float(request.form['height'])
+        topArea = pi * (r**2)
+        print(topArea)
+    return render_template('price.html', myValue= topArea)
 
 if __name__ == '__main__':
     app.run(debug=True)
